e726c5acff7d00e66b28ab79c10be404ea1acb31
imykhailychenko <igor.c.m@ukr.net>
2019-08-31 18:42:40 +0300

    scroll top

--- src/js/javaskript.js
+++ src/js/javaskript.js
@@ -4,3 +4,26 @@ btn.on('click', function () {
    $(this).toggleClass('header__btn--active');
    $(nav).toggleClass('nav--active');
 });
+
+$(document).ready(function () {
+  $('body').append('<a href="#" id="go-top" title="Вверх"><svg class="go-top__icon"><use xlink:href="./images/sprite.svg#top_arrow"></use></svg></a>');
+});
+
+$(function () {
+  $.fn.scrollToTop = function () {
+     $(this).hide().removeAttr("href");
+     if ($(window).scrollTop() >= "250") $(this).fadeIn("slow")
+     var scrollDiv = $(this);
+     $(window).scroll(function () {
+        if ($(window).scrollTop() <= "250") $(scrollDiv).fadeOut("slow")
+        else $(scrollDiv).fadeIn("slow")
+     });
+     $(this).click(function () {
+        $("html, body").animate({ scrollTop: 0 }, "slow")
+     })
+  }
+});
+
+$(function () {
+  $("#go-top").scrollToTop();
+});
